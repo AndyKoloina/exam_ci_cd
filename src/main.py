0c3bf6015110bbe73cd31f610dea848a39cb3c6d
Andy Ranaivo <andyranaivo861@gmail.com>
2026-03-15 18:13:49 +0000

    deploy to gcp

--- src/main.py
+++ src/main.py
@@ -1,19 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from . import smokeTest
-import os # Importez os
-
-# SUPPRIMEZ OU COMMENTEZ debugpy EN PROD
-# import debugpy
-# debugpy.listen(("0.0.0.0", 5678))
+import os
 
 app = FastAPI()
 
-origins = ["*"]
-
+# Configuration CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -23,7 +18,4 @@ app.include_router(smokeTest.router, prefix="/smoke-test")
 
 @app.get('/')
 def health():
-    return {
-        "message": "OK 🚀 lala",
-        "port_used": os.environ.get("PORT", "8080") # Pour vérifier le port
-    }
+    return {"status": "up", "message": "Deploy Success! 🚀"}
