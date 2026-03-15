6554ecb3229cb7e9701330e6143fbc1a5034d466
Andy Ranaivo <andyranaivo861@gmail.com>
2026-03-15 17:41:12 +0000

    deploy to gcp

--- app/main.py
+++
@@ -1,8 +0,0 @@
-import functions_framework
-
-@functions_framework.cloud_event
-def clean_data_trigger(cloud_event):
-    # Récupère les infos du fichier déposé
-    data = cloud_event.data
-    print(f"Fichier reçu : {data['name']} dans le bucket {data['bucket']}")
-    # Ta logique de Data Engineering ici
